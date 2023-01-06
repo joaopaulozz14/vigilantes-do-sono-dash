2dd21c44ef69545c40cdd6c24f5a696784d9db76
malkavianson <alexneto.best@gmail.com>
2023-01-05 23:16:16 -0300

    style: prettier indentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      Vigilantes do sono - Dashboard - KerannaYumi
-    </div>
-  );
+function App(): JSX.Element {
+	return (
+		<div className="App">Vigilantes do sono - Dashboard - KerannaYumi</div>
+	);
 }
 
 export default App;
