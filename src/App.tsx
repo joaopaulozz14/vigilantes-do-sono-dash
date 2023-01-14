cea6fa2161383341f3f897f211e0256d8f79236e
malkavianson <alexneto.best@gmail.com>
2023-01-13 23:14:23 -0300

    Feat: default json types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { IClientJSON } from "./@types";
 import "./App.css";
 
+const jsonData: IClientJSON = {};
+
 function App(): JSX.Element {
+	if (jsonData) {
+		console.log(jsonData);
+	}
 	return (
 		<div className="App">Vigilantes do sono - Dashboard - KerannaYumi</div>
 	);
