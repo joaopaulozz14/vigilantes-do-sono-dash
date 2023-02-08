b5b6ea745362dff41d4c8df7b223771823a8bed0
malkavianson <alexneto.best@gmail.com>
2023-02-08 20:15:07 -0300

    feat: add mocked data

---
+++ src/main/adapters/data.ts
@@ -0,0 +1,39 @@
+import { IChartData, IRating } from "@types";
+
+export const employee = {
+	name: "user",
+	password: "User*123",
+	email: "user@mail.com",
+};
+
+export const isi: IChartData = {
+	name: "Insonia",
+	improvement: 10,
+	values: [17, 10.7, 6.5],
+};
+export const gad: IChartData = {
+	name: "Ansiedade",
+	improvement: 10,
+	values: [4.2, 3.3, 1.8],
+};
+export const phq: IChartData = {
+	name: "Depressão",
+	improvement: 10,
+	values: [4.6, 3.18, 1.89],
+};
+export const prod: IChartData = {
+	name: "Produtividade",
+	improvement: 10,
+	values: [7.8, 3.4],
+};
+
+export const currentSessions = 3168;
+
+export const reportedNights = 8692;
+
+export const appliedTechniques = 3678;
+
+export const rating: IRating = {
+	ratingMedia: "4.61",
+	allRatings: [165, 235, 486, 728, 638],
+};
